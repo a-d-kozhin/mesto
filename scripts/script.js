9bb9565cc93d3b9647d3cc1b29e692240037cf3d
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-05-29 15:08:14 +0300

    hotfix: minor fixes

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ const elementTitleInput = document.querySelector('.popup__input_type_element-tit
 const elementUrlInput = document.querySelector('.popup__input_type_element-url');
 const popupProfile = document.querySelector('.popup-profile');
 const popupElement = document.querySelector('.popup-element');
-const elements = document.querySelector(".elements"); 
+const elements = document.querySelector(".elements");
 const elementTemplate = document.querySelector("#element");
 const popupWithImage = document.querySelector('.popup-image');
 const popupImage = document.querySelector('.popup-image__image');
@@ -21,28 +21,28 @@ const popupCaption = document.querySelector('.popup-image__caption');
 const elementImage = document.querySelector('.element__image');
 const initialElements = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
@@ -66,9 +66,9 @@ initialElements.forEach((item) => createElement(item.name, item.link));
 // до конца пока не разобрался, как ее применить в других функциях
 // удалось лишь повесить в конце на EventListener'ы
 function openClosePopup(popup) {
-  return function() { 
+  return function () {
     popup.classList.toggle('popup_opened');
-      if (popup === popupProfile && popup.classList.contains('popup_opened')) {
+    if (popup === popupProfile && popup.classList.contains('popup_opened')) {
       nameInput.value = profileName.textContent;
       jobInput.value = profileJob.textContent;
     }
@@ -77,16 +77,16 @@ function openClosePopup(popup) {
 // функция, реализующая лайки
 function toggleLike(evt) {
   if (evt.target.classList.contains('element__like-button')) {
-      const likeButtonOfElement = evt.target.closest('.element__like-button');
-      likeButtonOfElement.classList.add('element__like-button_active');
+    const likeButtonOfElement = evt.target.closest('.element__like-button');
+    likeButtonOfElement.classList.add('element__like-button_active');
   }
 }
 
 // функция для удаления карточек
 function removeElement(evt) {
   if (evt.target.classList.contains('element__remove-button')) {
-      const elementRemoveButton = evt.target.closest('.element__remove-button');
-      elementRemoveButton.parentNode.remove()
+    const elementRemoveButton = evt.target.closest('.element__remove-button');
+    elementRemoveButton.parentNode.remove()
   }
 }
 
@@ -100,31 +100,31 @@ function getImage(evt) {
 }
 
 // функция-обработчик формы заполнения профиля
-function formSubmitHandlerProfile (evt) {
+function formSubmitHandlerProfile(evt) {
   evt.preventDefault();
   let nameInputValue = nameInput.value.trim();
-	let jobInputValue = jobInput.value.trim();
+  let jobInputValue = jobInput.value.trim();
   profileName.textContent = nameInputValue;
-	profileJob.textContent = jobInputValue;
+  profileJob.textContent = jobInputValue;
   popupProfile.classList.toggle('popup_opened');
 }
 
 // функция-обработчик формы создания карточки
-function formSubmitHandlerElement (evt) {
+function formSubmitHandlerElement(evt) {
   evt.preventDefault();
   createElement(elementTitleInput.value.trim(), elementUrlInput.value.trim());
   popupElement.classList.toggle('popup_opened');
 }
 
-// попап profile
+// слушатели попапа profile
 profileEditButton.addEventListener('click', openClosePopup(popupProfile));
 popupProfileCloseButton.addEventListener('click', openClosePopup(popupProfile));
 profileForm.addEventListener('submit', formSubmitHandlerProfile);
 
-//попап element
+// слушатели попапа element
 profileAddButton.addEventListener('click', openClosePopup(popupElement));
 popupElementCloseButton.addEventListener('click', openClosePopup(popupElement));
 elementForm.addEventListener('submit', formSubmitHandlerElement);
 
-// попап с картинкой
+// слушатели попапа с картинкой
 popupWithImageCloseButton.addEventListener('click', openClosePopup(popupWithImage));
