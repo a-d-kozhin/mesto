ebd1cdb955180e1f17878bdb84e4941e39fdeefd
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-05-29 14:48:27 +0300

    refactor: make createElement, get rid of 2 separate functions; fix: animation issue

--- scripts/script.js
+++ scripts/script.js
@@ -13,57 +13,12 @@ const elementTitleInput = document.querySelector('.popup__input_type_element-tit
 const elementUrlInput = document.querySelector('.popup__input_type_element-url');
 const popupProfile = document.querySelector('.popup-profile');
 const popupElement = document.querySelector('.popup-element');
-
-
-function openClosePopup(popup) {
-  return function() { 
-    popup.classList.toggle('popup_opened');
-      if (popup === popupProfile && popup.classList.contains('popup_opened')) {
-      popup.classList.toggle('open-smoothly');
-      nameInput.value = profileName.textContent;
-      jobInput.value = profileJob.textContent;
-    }
-    if (popup === popupElement) {
-      popup.classList.toggle('open-smoothly')};
-  }
-}
-
-function formSubmitHandlerProfile (evt) {
-  evt.preventDefault();
-  let nameInputValue = nameInput.value.trim();
-	let jobInputValue = jobInput.value.trim();
-  profileName.textContent = nameInputValue;
-	profileJob.textContent = jobInputValue;
-  popupProfile.classList.toggle('popup_opened');
-}
-
-function formSubmitHandlerElement (evt) {
-  evt.preventDefault();
-  let elementTitleInputValue = elementTitleInput.value.trim();
-  let elementUrlInputValue = elementUrlInput.value.trim();
-  const userElement = elementTemplate.cloneNode(true);
-  userElement.querySelector('.element__image').src = elementUrlInputValue;
-  userElement.querySelector('.element__title').textContent = elementTitleInputValue;
-  userElement.querySelector('.element__image').alt = elementTitleInputValue;
-  const userElementRemoveButton = userElement.querySelector('.element__remove-button');
-  const userElementLike = userElement.querySelector('.element__like-button')
-  const userElementImage = userElement.querySelector('.element__image')
-  userElementLike.addEventListener('click', toggleLike);
-  userElementRemoveButton.addEventListener('click', removeElement);
-  popupElement.classList.toggle('popup_opened');
-  userElementImage.addEventListener('click', getImage);
-  elements.prepend(userElement);
-}
-
-profileEditButton.addEventListener('click', openClosePopup(popupProfile));
-popupProfileCloseButton.addEventListener('click', openClosePopup(popupProfile));
-profileForm.addEventListener('submit', formSubmitHandlerProfile);
-
-profileAddButton.addEventListener('click', openClosePopup(popupElement));
-popupElementCloseButton.addEventListener('click', openClosePopup(popupElement));
-elementForm.addEventListener('submit', formSubmitHandlerElement);
-//////////////////////////////////////////////////////////////////
-
+const elements = document.querySelector(".elements"); 
+const elementTemplate = document.querySelector("#element");
+const popupWithImage = document.querySelector('.popup-image');
+const popupImage = document.querySelector('.popup-image__image');
+const popupCaption = document.querySelector('.popup-image__caption');
+const elementImage = document.querySelector('.element__image');
 const initialElements = [
   {
       name: 'Архыз',
@@ -91,23 +46,35 @@ const initialElements = [
   }
 ];
 
-const elements = document.querySelector(".elements");
-const elementTemplate = document.querySelector("#element").content;
-
-for (let i = 0; i < initialElements.length; i += 1) {
-  const initialElement = elementTemplate.cloneNode(true);
-  initialElement.querySelector('.element__image').src = initialElements[i].link;
-  initialElement.querySelector('.element__title').textContent = initialElements[i].name;
-  initialElement.querySelector('.element__image').alt = initialElements[i].name;
-  elements.append(initialElement);
+// функция создания карточек
+function createElement(elementName, elementLink) {
+  const element = elementTemplate.content.cloneNode(true);
+  element.querySelector('.element__image').src = elementLink;
+  element.querySelector('.element__title').textContent = elementName;
+  element.querySelector('.element__image').alt = elementName;
+  const elementContainer = element.querySelector('.element')
+  elementContainer.addEventListener('click', removeElement)
+  elementContainer.addEventListener('click', toggleLike)
+  elementContainer.addEventListener('click', getImage)
+  elements.prepend(element);
 }
 
-const popupWithImage = document.querySelector('.popup-image');
-const popupImage = document.querySelector('.popup-image__image');
-const popupCaption = document.querySelector('.popup-image__caption');
-const elementContainers = document.querySelectorAll('.element');
-const elementImage = document.querySelector('.element__image');
+// создаем карточки из массива
+initialElements.forEach((item) => createElement(item.name, item.link));
 
+// функция открытия-закрытия попапов
+// до конца пока не разобрался, как ее применить в других функциях
+// удалось лишь повесить в конце на EventListener'ы
+function openClosePopup(popup) {
+  return function() { 
+    popup.classList.toggle('popup_opened');
+      if (popup === popupProfile && popup.classList.contains('popup_opened')) {
+      nameInput.value = profileName.textContent;
+      jobInput.value = profileJob.textContent;
+    }
+  }
+}
+// функция, реализующая лайки
 function toggleLike(evt) {
   if (evt.target.classList.contains('element__like-button')) {
       const likeButtonOfElement = evt.target.closest('.element__like-button');
@@ -115,6 +82,7 @@ function toggleLike(evt) {
   }
 }
 
+// функция для удаления карточек
 function removeElement(evt) {
   if (evt.target.classList.contains('element__remove-button')) {
       const elementRemoveButton = evt.target.closest('.element__remove-button');
@@ -122,6 +90,7 @@ function removeElement(evt) {
   }
 }
 
+// функция, получающая содержимое для попапа с изображением
 function getImage(evt) {
   if (evt.target.classList.contains('element__image')) {
     popupWithImage.classList.toggle('popup_opened');
@@ -130,7 +99,32 @@ function getImage(evt) {
   }
 }
 
-elementContainers.forEach(element => element.addEventListener('click', removeElement)); 
-elementContainers.forEach(element => element.addEventListener('click', toggleLike));
-elementContainers.forEach(element => element.addEventListener('click', getImage));
+// функция-обработчик формы заполнения профиля
+function formSubmitHandlerProfile (evt) {
+  evt.preventDefault();
+  let nameInputValue = nameInput.value.trim();
+	let jobInputValue = jobInput.value.trim();
+  profileName.textContent = nameInputValue;
+	profileJob.textContent = jobInputValue;
+  popupProfile.classList.toggle('popup_opened');
+}
+
+// функция-обработчик формы создания карточки
+function formSubmitHandlerElement (evt) {
+  evt.preventDefault();
+  createElement(elementTitleInput.value.trim(), elementUrlInput.value.trim());
+  popupElement.classList.toggle('popup_opened');
+}
+
+// попап profile
+profileEditButton.addEventListener('click', openClosePopup(popupProfile));
+popupProfileCloseButton.addEventListener('click', openClosePopup(popupProfile));
+profileForm.addEventListener('submit', formSubmitHandlerProfile);
+
+//попап element
+profileAddButton.addEventListener('click', openClosePopup(popupElement));
+popupElementCloseButton.addEventListener('click', openClosePopup(popupElement));
+elementForm.addEventListener('submit', formSubmitHandlerElement);
+
+// попап с картинкой
 popupWithImageCloseButton.addEventListener('click', openClosePopup(popupWithImage));
