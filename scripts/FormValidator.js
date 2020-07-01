3240d18a970c2c99bbbaed27f9c6958b62afd608
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-07-01 17:05:20 +0300

    refactor: add comments

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,3 +1,4 @@
+// класс валидации
 export class FormValidator {
   constructor(options, formElement) {
     this._formElement = document.querySelector(formElement);
@@ -9,7 +10,7 @@ export class FormValidator {
     this._formSelector = options.formSelector;
     this._inputElements = Array.from(this._formElement.querySelectorAll(this._inputSelector));
   }
-
+  // приватный метод обработки инпута
   _handleInput(e) {
     this._input = e.target
     this._inputIsValid = this._input.checkValidity();
@@ -20,21 +21,21 @@ export class FormValidator {
       this._showErrorMessage(this._input, this._input.validationMessage);
     }
   }
-  
+  // приватный метод для показа системных ошибок
   _showErrorMessage(input, errorMessage) {
     const errorElement = document.querySelector(`#${input.id}-error`);
     errorElement.classList.add(this._errorClass);
     errorElement.textContent = errorMessage;
     input.classList.add(this._inputErrorClass);
   }
-
+  // приватный метод для скрытия системных ошибок
   _hideErrorMessage(input) {
     const errorElement = document.querySelector(`#${input.id}-error`);
     errorElement.classList.remove(this._errorClass);
     errorElement.textContent = '';
     input.classList.remove(this._inputErrorClass);
   }
-
+  // приватный метод для включения/отключения сабмита формы
   _toggleSubmitState() {
     const submit = this._formElement.querySelector(this._submitButtonSelector);
     const formIsInvalid = !this._formElement.checkValidity();
@@ -47,15 +48,14 @@ export class FormValidator {
       submit.disabled = false;
     }
   }
-
+  // публичный метод для очистки формы и ошибок валидации
   resetForm() {
     this._inputElements.forEach(element => this._hideErrorMessage(element));
     this._formElement.reset();
   }
-
+  // публичный метод, включающий валидацию формы
   enableValidation() {
       this._inputElements.forEach(element => {element.addEventListener('input', (event) => { this._handleInput(event) }) })
       this._formElement.addEventListener('input', () => { this._toggleSubmitState() })    
   }
-
 }
